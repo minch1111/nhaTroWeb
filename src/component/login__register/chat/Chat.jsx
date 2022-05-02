1dfc005158a3be0595fb639c9252db748df673c1
pet-app-minch <30566942+minch1111@users.noreply.github.com>
2022-05-02 16:08:26 +0700

    update code 2

--- src/component/login__register/chat/Chat.jsx
+++ src/component/login__register/chat/Chat.jsx
@@ -4,7 +4,7 @@ import { ChatBox } from 'react-chatbox-component';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import useForm from '../../../hooks/useForm';
 import { Socket, io } from 'socket.io-client';
-import { chatAPI } from '../../../config/api';
+import { apiWithoutUser, chatAPI } from '../../../config/api';
 import { Context } from '../../../App';
 let socket
 // import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
@@ -30,7 +30,7 @@ export default function Chat(props) {
   useEffect(() => {
     // const { name, room } = queryString.parse(location.search);
 
-    socket = io(chatAPI, {
+    socket = io(apiWithoutUser, {
       // withCredentials:true,
     });
 
@@ -83,24 +83,17 @@ export default function Chat(props) {
   const submit = (e) => {
     e.preventDefault();
     let currentId = user._id
+    console.log("run");
     // console.log('currentId', currentId)
     socket.emit('sendMessage', { IdSender: currentId, IdReceiver: '626f55060aebe71d0817cb7b', message: message, socketId: socket.id })
+    setMessages([...messages,message])
     setMessage('')
   }
   const { form, handleSubmit, error, register } = useForm()
   const arr = [1, 2, 3, 4, 5]
-  console.log('messages', messages)
+  // console.log('messages', messages)
   return (
-    <div className='chat boxF border border-dark' style={{ backgroundColor: '#f3f3f3', minWidth: '300px' }} >
-      {/* <Widget
-        handleNewUserMessage={handleNewUserMessage}
-        // profileAvatar={logo}
-        title="My new awesome title"
-        subtitle="And my cool subtitle"
-      /> */}
-      {/* <ChatBox
-      messages={messages}
-      /> */}
+    <div className='chat boxF shadow-sm' style={{ backgroundColor: '#f3f3f3', minWidth: '300px' }} >
       <div className='title-name px-2 w-100 bg-info d-flex align-items-center justify-content-between' style={{ height: '45px' }}>
         <div className='Name text-light'><strong>Huy Lê</strong></div>
         <div className='cursor-pointer' onClick={() => closeChat()}> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" fontSize={12} width={15}><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" /></svg></div>
